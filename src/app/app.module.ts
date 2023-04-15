58aeefbce6cf5d90b34e3eb0116b7a9070cf77a9
adda issa abdoul razak <addaissaabdoulrazak@gmail.com>
2023-04-15 17:41:10 +0100

    how to break down a component and additional of two components(home + nav-bar + products )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
